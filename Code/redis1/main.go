7ecea952bfee6c7a66fea14d70a78f9673f42131
noonespecial009 <noonespecial009@gmail.com>
2019-01-20 16:09:09 -0800

    first commit

---
+++ Code/redis1/main.go
@@ -0,0 +1,39 @@
+/*
+brew install redis
+brew services start redis
+cd redis1 (just a random folder name)
+go build
+./redis1
+*/
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/astaxie/goredis"
+)
+
+func main() {
+	var client goredis.Client
+
+	// Set the default port in Redis
+	client.Addr = "127.0.0.1:6379"
+
+	// string manipulation
+	client.Set("a", []byte("hello"))
+	val, _ := client.Get("a")
+	fmt.Println(string(val))
+	client.Del("a")
+
+	// list operation
+	vals := []string{"a", "b", "c", "d", "e"}
+	for _, v := range vals {
+		client.Rpush("l", []byte(v))
+	}
+	dbvals, _ := client.Lrange("l", 0, 4)
+	for i, v := range dbvals {
+		println(i, ":", string(v))
+	}
+	client.Del("l")
+}
